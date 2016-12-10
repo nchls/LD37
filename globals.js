bcf4b5590ca6603613168613d7ede53fd8b48d31
Ian <ian@nchls.com>
2016-12-09 21:55:44 -0500

    Initial commit

---
+++ globals.js
@@ -0,0 +1,34 @@
+window.globals = {
+	screenWidth: 600,
+	screenHeight: 600,
+
+	assets: {
+		graphs: {
+
+		}
+	},
+
+	fonts: {
+		main: {
+			font: '20px Consolas, monospace',
+			fill: '#fff',
+			align: 'center'
+		},
+		console: {
+			font: '20px Consolas, monospace',
+			fill: '#fff',
+			align: 'left'
+		},
+	},
+
+	controls: {
+		left: 'LEFT',
+	},
+
+	collectMode: 'gravity'
+};
+
+window.state = {
+	processRegistry: {},
+	controls: {}
+};
