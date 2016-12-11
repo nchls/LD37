a86172e9d2aea71b137ecac216160babd3cabc13
ebenpack <ebenpackwood@gmail.com>
2016-12-11 15:11:42 -0500

    Dammit, add those backticks

--- server/room.js
+++ server/room.js
@@ -4,5 +4,5 @@ exports.createRoom = function(request, reply) {
 
 exports.getRoom = function(request, reply) {
     var roomId = request.params.roomId;
-    reply('returning room ${roomId}');
+    reply(`returning room ${roomId}`);
 };
